59b69006bfb14eb3b360f797089c9407c8a40df3
Michael Mitchell <michael.mitchell@g5searchmarketing.com>
2013-07-15 15:47:02 -0700

    adds require jquery to application so asset pipeline can find it

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ require "action_mailer/railtie"
 # require "active_resource/railtie"
 require 'mongoid/railtie'
 require "sprockets/railtie"
+require "jquery-rails"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
